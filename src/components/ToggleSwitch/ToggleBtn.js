23bbbec46c3dfcbf8fa538f7949c7bec239f0cd7
SangHoon <joa3668@gmail.com>
2022-01-28 02:42:15 +0900

    Add: 토글 boolean 값 state 끌어올리기 함수 추가

--- src/components/ToggleSwitch/ToggleBtn.js
+++ src/components/ToggleSwitch/ToggleBtn.js
@@ -1,13 +1,11 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import * as S from './Toggle.style';
 
 export const ToggleBtn = ({ onChange }) => {
-  const [isActive, setIsActive] = useState(false);
-
   const handleCheckBox = e => {
     const { checked } = e.target;
-    setIsActive(checked);
+    onChange(checked);
   };
 
   return (
