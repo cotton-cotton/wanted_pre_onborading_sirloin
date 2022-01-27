69ed8282d2814cdc79db0e565799230536013f18
SangHoon <joa3668@gmail.com>
2022-01-27 23:03:44 +0900

    Modify: 토글버튼 디자인 수정

--- src/components/ToggleSetting/ToggleBtn.js
+++ src/components/ToggleSetting/ToggleBtn.js
@@ -20,7 +20,7 @@ const SwitchWrapper = styled.div`
 const CheckLabel = styled.label`
   position: relative;
   display: block;
-  width: 45px;
+  width: 40px;
 `;
 
 const Switch = styled.span`
@@ -30,7 +30,7 @@ const Switch = styled.span`
   left: 0;
   right: 0;
   bottom: 0;
-  height: 15px;
+  height: 17px;
   background-color: ${props => props.theme.color.gray};
   border-radius: 20px;
   transition: 0.4s;
@@ -40,7 +40,7 @@ const Switch = styled.span`
     position: absolute;
     width: 22px;
     height: 22px;
-    top: -3px;
+    top: -2px;
     border-radius: 50%;
     box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
     background-color: white;
@@ -50,7 +50,7 @@ const Switch = styled.span`
 
 const InputCheck = styled.input`
   opacity: 0;
-  width: 45px;
+  width: 40px;
   height: 17px;
 
   &:checked + ${Switch} {
@@ -59,7 +59,7 @@ const InputCheck = styled.input`
 
     &::before {
       background-color: #2919be;
-      transform: translateX(24px);
+      transform: translateX(19px);
       transition: 0.4s;
     }
   }
