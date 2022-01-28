a369205c0989872aeb15539fde4a67fc404d3bdd
eojine94 <eojine294@gmail.com>
2022-01-28 17:06:05 +0900

    Add : 상품 배송 설정 component layout 구현

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import styled from 'styled-components';
 
-// import { ExpoSalesPeriodSetting } from '../components/ExpoSalesPeriodSetting/ExpoSalesPeriodSetting.js';
+import ExpoSalesPeriodSetting from '../components/ExpoSalesPeriodSetting/ExpoSalesPeriodSetting.js';
 // import { ProdBasicInfo } from '../components/ProdBasicInfo/ProdBasicInfo.js';
 // import { ProdOption } from '../components/ProdOption/ProdOption.js';
 // import { ProdDescImg } from '../components/ProdDescImg/ProdDescImg.js';
 // import { SalerRecImg } from '../components/SalerRecImg/SalerRecImg.js';
 // import { ProdInfoNotice } from '../components/ProdInfoNotice/ProdInfoNotice.js';
-// import { ProdDeliverySetting } from '../components/ProdDeliverySetting/ProdDeliverySetting.js';
+import ProdDeliverySetting from '../components/ProdDeliverySetting/ProdDeliverySetting.js';
 import { ToggleSetting } from '../components/ToggleSetting/ToggleSetting.js';
 
 function Main() {
   return (
     <Wrapper>
-      {/* <ExpoSalesPeriodSetting />
-      <ProdBasicInfo />
+      <ExpoSalesPeriodSetting />
+      {/* <ProdBasicInfo />
       <ProdOption />
       <ProdDescImg />
       <SalerRecImg />
-      <ProdInfoNotice />
-      <ProdDeliverySetting /> */}
+      <ProdInfoNotice /> */}
+      <ProdDeliverySetting />
       <ToggleSetting title="상품 혜택 허용 설정" content="마일리지 적립" />
       <ToggleSetting title="기타 설정" content="감사카드 제공" />
     </Wrapper>
